9d5fb6078f5aebb09964c3fe48f64ecef3a20837
Nguyễn Hữu Khánh <37452723+Nguyenkhanh98@users.noreply.github.com>
2025-04-24 02:23:20 +0700

    remove

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "onair-tf"
-    key            = "prod/terraform-public.tfstate"
-    region         = "ap-southeast-1"
-    dynamodb_table = "terraform-lock-2"
-  }
-}
--- cloudwatch.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_cloudwatch_log_group" "onair_logs_public" {
-  name              = "/ecs/onair_public"
-  retention_in_days = 30
-}
